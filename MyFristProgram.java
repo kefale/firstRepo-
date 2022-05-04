08d2a1671c41d7a0c7d48acd5067a5a6bfe31ec2
kefale <kefalemulatu98@gmail.com>
2022-05-04 14:22:01 +0300

    last commit

---
+++ MyFristProgram.java
@@ -0,0 +1,25 @@
+class MyFirstProgram{
+
+/* This is my frist peogram in java 
+* this will print 'Hello World' as output
+*/
+public static void main(String[] args){
+
+System.out.println("Hello World");
+}
+}
+/* what are idnetifiers in java?
+* all java commponents needs names this names for classes, methods and variable  * are known as identifiers 
+* Idntifiers should begin in A - Z , a - z , $(dolar sign) or _(under score )
+* keywods in java should not  be used as an identifir 
+* most importantly identifiers are case  sensitive 
+* Examples of legal idntifiers $ke23, _kefale, abebe, Neba
+* Examples of illegal identifiers.123kefe, &kmm. -mm, 
+*
+*
+*
+*
+*
+*
+*
+*/
--- Runner.java
+++ Runner.java
@@ -1,6 +1,25 @@
-class Runner
+package git-Demo.com;
+import util.*;
+import util.scanner;
+public class Runner
 {
-public static void main(String[] args){
+public static void main(String[] args)
+{
 System.out.println("Runner");
 }
+
 }
+class {
+    int n;
+    int [] a = [1,2,3,4,5,6];
+    for(int i =0; i <= n; i++){
+
+        int x[] = new int a[];
+        if(i >1){
+            x[i] = a[i -1];
+        }
+        else {
+            x[i] = a[i];
+        }
+        }
+        }
